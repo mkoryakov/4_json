a3161960ccf6c169ee29046708a04bed1695ed7c
mkoryakov <mnkoryakov@yandex.ru>
2016-10-07 13:03:57 +0000

    На проверку

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
 import json
+import argparse
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as jsonObj:
+        data = json.load(jsonObj)
+    return data
 
 
 def pretty_print_json(data):
-    pass
-
+    for d in data:
+        print(json.dumps(data[0], ensure_ascii=False, indent=4))
+    
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(
+        description='Удобный для чтения вывод информации в формате json')
+    parser.add_argument('--json_file_name', '-jsf', type=str, 
+                            default='bars.json', help='Имя файла json')
+    args = parser.parse_args()
+    data = load_data(args.json_file_name)
+    pretty_print_json(data)
