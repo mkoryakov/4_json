c5b142c347c7ea86170c9678d82f2ecb932d0701
mkoryakov <mnkoryakov@yandex.ru>
2016-10-07 14:38:00 +0000

    Исправил. Имя какой переменной не понравилось?

--- pprint_json.py
+++ pprint_json.py
@@ -9,14 +9,13 @@ def load_data(filepath):
 
 
 def pretty_print_json(data):
-    for d in data:
-        print(json.dumps(data, ensure_ascii=False, indent=4))
+    print(json.dumps(data, ensure_ascii=False, indent=4))
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Удобный для чтения вывод информации в формате json')
-    parser.add_argument('--json_file_name', '-jsf', type=str, 
+    parser.add_argument('--json_file_name', '-jfn', type=str, 
                             default='bars.json', help='Имя файла json')
     args = parser.parse_args()
     data = load_data(args.json_file_name)
